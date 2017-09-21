edf6b9e8604183663a0bd9318caf92f2ca8ddd12
Fabio Bertinatto <fbertina@redhat.com>
2017-09-21 17:10:19 +0200

    Add basic config for packaging

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: clean
+clean:
+	@rm -rf build/ dist/ *.egg-info
+	@find . -name '__pycache__' -exec rm -fr {} +
+	@find . -name '*.pyc' -exec rm -f {} +
+	@find . -name '*.pyo' -exec rm -f {} +
+
+.PHONY: install
+install:
+	python setup.py install
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='leappctl',
+    version='0.0.1',
+    description='A control interface for leapp-daemon',
+    long_description=long_description,
+    url='https://github.com/leapp-to/leappctl',
+    author='Red Hat',
+    author_email='leapp-devel@redhat.com',
+    license="GPLv2+",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['click'],
+    entry_points={
+        'console_scripts': [
+            'leappctl=leappctl.cli:cli',
+        ],
+    },
+)
