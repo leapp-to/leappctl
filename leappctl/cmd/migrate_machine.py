566634a6fa4e7aa36f26c6fbf67f52c92a0083f3
Fabio Bertinatto <fbertina@redhat.com>
2017-09-25 12:53:58 +0200

    Basic communication with daemon

--- leappctl/cmd/migrate_machine.py
+++ leappctl/cmd/migrate_machine.py
@@ -1,12 +1,11 @@
 import click
-import requests
-from requests.exceptions import ConnectionError
 
-from leappctl import __version__
+from leappctl.session import get, post
 from leappctl.utils import port_spec
 
 
-CMD_SHORT_HELP = """Executes the migration of an OS into a macrocontainer"""
+CMD = "migrate-machine"
+CMD_SHORT_HELP = "Executes the migration of an OS into a macrocontainer"
 CMD_LONG_HELP = """
 This command migrates one or more application into containers by creating a macrocontainer.
 
@@ -14,26 +13,67 @@ This means that the entire system will be converted into a container, possibly b
 """
 
 
-@click.command('migrate-machine', help=CMD_LONG_HELP, short_help=CMD_SHORT_HELP)
-@click.option('--source-host', '-s', required=True, prompt=True, help='Migrated container will not be started immediately.')
-@click.option('--source-user', '-u', required=True, default='root', help='User in source host to connect via SSH.')
-@click.option('--target-host', '-t', required=True, default='localhost', help='Target host in which macrocontainer will run.')
-@click.option('--target-user', '-U', default='root', help='User in target host to connect via SSH.')
-@click.option('--container-name', '-n', help='Container name to be created in the target host.')
-@click.option('--disable-start', '-d', is_flag=True, default=False, help='Don\'t start the container')
-@click.option('--force-create', '-f', is_flag=True, default=True, help='Force container creation in the target.')
-@click.option('--excluded_paths', '-E', multiple=True, type=click.Path(), help='Define paths which will be excluded from the source')
-@click.option('--tcp-port', '-p', default=None, type=port_spec, help='(Re)define target tcp ports to forward to macrocontainer - [target_port:source_port]')
-@click.option('--excluded-port', '-e', default=None, type=port_spec, help='Define tcp ports which will be excluded from the mapped ports [[target_port]:source_port>]')
-@click.option('--debug', '-D', is_flag=True, default=False, help='Turn on debug logging on stderr')
+@click.command(CMD, help=CMD_LONG_HELP, short_help=CMD_SHORT_HELP)
+@click.option('--source-host',
+              '-s',
+              required=True,
+              prompt=True,
+              help='Host to be migrated into a macrocontainer.')
+@click.option('--source-user',
+              '-u',
+              required=True,
+              default='root',
+              help='User in source host to connect via SSH.')
+@click.option('--target-host',
+              '-t',
+              required=True,
+              prompt=True,
+              default='localhost',
+              help='Host in which macrocontainer will run.')
+@click.option('--target-user',
+              '-U',
+              default='root',
+              help='User in target host to connect via SSH.')
+@click.option('--container-name',
+              '-n',
+              help='Container name to be created in the target host.')
+@click.option('--disable-start',
+              '-d',
+              is_flag=True,
+              default=False,
+              help='Don\'t start the container')
+@click.option('--force-create',
+              '-f',
+              is_flag=True,
+              default=True,
+              help='Force container creation in the target.')
+@click.option('--excluded_paths',
+              '-E',
+              multiple=True,
+              type=click.Path(),
+              help='Define paths which will be excluded from the source')
+@click.option('--tcp-port',
+              '-p',
+              default=None,
+              type=port_spec,
+              help='(Re)define target tcp ports to forward to macrocontainer - [target_port:source_port]')
+@click.option('--excluded-port',
+              '-e',
+              default=None,
+              type=port_spec,
+              help='Define tcp ports which will be excluded from the mapped ports [[target_port]:source_port>]')
+@click.option('--debug',
+              '-D',
+              is_flag=True,
+              default=False,
+              help='Turn on debug logging on stderr')
 def cli(**kwargs):
-    with requests.Session() as session:
-        headers = {
-            'User-Agent': 'leappctl/{version}'.format(version=__version__),
-        }
-        try:
-            resp = session.get('http://localhost:8000', headers=headers, json=kwargs)
-        except ConnectionError:
-            click.secho('Connection error', fg='red', bold=True)
-        else:
-            click.secho('Response: {}\n'.format(resp.text.encode('utf8')), fg='green', bold=True)
+    # Simple POST
+    resp = post(CMD, kwargs)
+    data = resp.json()
+    click.echo('Response: {0}\n'.format(data['json']['target_host']))
+
+    # Simple GET
+    resp1 = get(CMD, kwargs)
+    data = resp1.json()
+    click.echo('Response: {0}\n'.format(data))
---
+++ leappctl/session.py
@@ -0,0 +1,36 @@
+import os
+from contextlib import contextmanager
+
+import requests
+
+from leappctl import __version__
+from leappctl.compat import urljoin
+
+
+def make_endpoint(cmd):
+    BASE_ENDPOINT = os.getenv('LEAPP_DAEMON_ENDPOINT', 'http://127.0.0.1')
+    return urljoin(BASE_ENDPOINT, cmd)
+
+
+@contextmanager
+def get_daemon_session():
+    """Set default HTTP parameters to communicate with daemon"""
+    headers = {
+        'User-Agent': 'leappctl/{version}'.format(version=__version__),
+    }
+
+    with requests.Session() as session:
+        session.headers.update(headers)
+        yield session
+
+
+def get(cmd, params):
+    """Send a GET request to the daemon"""
+    with get_daemon_session() as s:
+        return s.get(make_endpoint(cmd), json=params)
+
+
+def post(cmd, params):
+    """Send a POST request to the daemon"""
+    with get_daemon_session() as s:
+        return s.post(make_endpoint(cmd), json=params)
