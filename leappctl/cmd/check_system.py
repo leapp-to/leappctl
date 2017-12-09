d62eb349cf6f62521c31879110283049afa72b1d
Arthur Mello <amello@redhat.com>
2017-12-09 02:29:16 +0000

    Added support for getting HTML result from checks
    - Added --html option to check-system so user can get an HTML output from checks results
    - Added --out option to check-system so user can decide to save result into a file

--- leappctl/cmd/check_system.py
+++ leappctl/cmd/check_system.py
@@ -7,6 +7,7 @@ from leappctl.session import post
 
 CMD = "check-system"
 CMD_HELP = "Run a group of checks on the system"
+HTML_OUT_CMD = "check-html-output"
 
 
 @click.command(CMD, help=CMD_HELP)
@@ -15,17 +16,48 @@ CMD_HELP = "Run a group of checks on the system"
               required=True,
               prompt=True,
               help='Checks to be executed on system.')
+@click.option('--html',
+              is_flag=True,
+              default=False,
+              help='Display check result as HTML')
+@click.option('--out',
+              '-o',
+              help='File where result should be stored')
 def cli(**kwargs):
     req_body = kwargs
+
+    display_html = req_body.pop('html')
+    output_file = req_body.pop('out')
 
     # POST collected data to the appropriate endpoint in leapp-daemon
     resp = post(CMD, req_body)
 
     # Pretty-print response
     resp_body = resp.json()
-    json_pprint = json.dumps(resp_body, sort_keys=True, indent=4, separators=(',', ': '))
-    click.secho(
-        'Response:\n{0}\n'.format(json_pprint),
-        bold=True,
-        fg='green' if resp.status_code == 200 else 'red'
-    )
+
+    if display_html:
+        html_content = ""
+        resp_html = post(HTML_OUT_CMD, resp_body['data'])
+        resp_html_body = resp_html.json()
+        if 'html_output' in resp_html_body['data']:
+            html_output = resp_html_body['data']['html_output']
+            if html_output:
+                html_content = html_output[0]['value']
+
+        if output_file:
+            with open(output_file, 'w+') as f:
+                f.write(html_content)
+        else:
+            print(html_content)
+
+    else:
+        json_pprint = json.dumps(resp_body, sort_keys=True, indent=4, separators=(',', ': '))
+        if output_file:
+            with open(output_file, 'w+') as f:
+                f.write('Response:\n{0}\n'.format(json_pprint))
+        else:
+            click.secho(
+                'Response:\n{0}\n'.format(json_pprint),
+                bold=True,
+                fg='green' if resp.status_code == 200 else 'red'
+            )
